28f441e3b55223884de73b9a76b99a2be7d8e009
leireabajas <leire.abajasaldea@cieep.com>
2024-05-03 21:56:15 +0200

    HE creado el ccomponente notfound y he estado actualizando css y html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,15 +11,18 @@ import { ImpresoraComponent } from './componentes/catalogo/impresora/impresora.c
 import { RegistroComponent } from './componentes/registro/registro.component';
 import { LoginComponent } from './componentes/login/login.component';
 import { CarritoComponent } from './componentes/carrito/carrito.component';
+import { NotFoundComponent } from './componentes/not-found/not-found.component';
 
 // Importa otros componentes necesarios
 
 export const routes: Routes = [
+  
+  
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component:HomeComponent },
   { path: 'catalogo', component: CatalogoComponent },
 
-  
+  { path: 'notfound', component: NotFoundComponent },
   { path: 'contacto', component: ContactoComponent },
   { path: 'moviles', component: MovilesComponent },
   { path: 'relojes', component: RelojesComponent },
